9fd60ea9f0dc993a707410b87788368345b83fd9
Sharmina Akhtar <sakht318@gmail.com>
2018-05-29 04:22:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function prependDriver(name){
 }
 
 function removeLastDriver(){
-  return drivers.slice(0, days.length - 1);
+  return drivers.slice(0, drivers.length - 1);
 }
 
 function removeFirstDriver(){
