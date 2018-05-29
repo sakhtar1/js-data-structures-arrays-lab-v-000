f4315961499b4ddf5e04df395ac10e0191a70d09
Sharmina Akhtar <sakht318@gmail.com>
2018-05-29 04:08:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 // Write your solution here!
 const drivers = ["Milo", "Otis", "Garfield"];
+
 function destructivelyAppendDriver(name){
   drivers.push("Ralph");
 }
+
 function destructivelyPrependDriver(name){
   drivers.unshift("Bob");
+}
+
+function destructivelyRemoveLastDriver(){
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver(){
+  drivers.shift();
 }
